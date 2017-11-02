b30a69a5018d42c1409b7192d7d518beda3cf5ee
Ludwig <ludwig.f.gustafsson@gmail.com>
2017-11-02 11:41:01 +0100

    c

--- src/todolist/ToDoItem.java
+++ src/todolist/ToDoItem.java
@@ -1,4 +1,5 @@
 package todolist;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -6,59 +7,85 @@ public class ToDoItem
 {
 	private LocalDateTime startDate;
 	private LocalDateTime endDate;
-	
+
 	private String activityTitle;
 	private String description;
 	private Status status;
 
 	private static int counter = 0;
 	private int id = 0;
+
 	public ToDoItem(String activityName, String description, LocalDateTime deadlineDate)
 	{
 		super();
 		counter++;
-		this.id=counter;
+		this.id = counter;
 		this.startDate = LocalDateTime.now();
 		this.status = Status.OPEN;
 		this.activityTitle = activityName;
 		this.description = description;
 		this.endDate = deadlineDate;
 	}
-	
-	public  int getId()
+
+	public int getId()
 	{
 		return id;
 	}
+
 	public String getActivityTitle()
 	{
 		return this.activityTitle;
 	}
-	
+
 	public void setActivityTitle(String activityName)
 	{
 		this.activityTitle = activityName;
 	}
-	
+
 	public String getDescription()
 	{
 		return description;
 	}
-	
+
 	public void setDescription(String description)
 	{
-		
+
 		this.description = description;
 	}
-	
 
 	@Override
-	public String toString() {
-		 DateTimeFormatter formatter =   DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
-		return id + " startDate: " + startDate.format(formatter) + ", endDate: " + endDate.format(formatter) + ", activityName: " + activityTitle
-				+ ", description: " + description + ", status:" + status + "";
+	public String toString()
+	{
+		int t = 10 - activityTitle.length();
+		StringBuilder buffer = new StringBuilder();
+		int i = 0;
+		while (i < t)
+		{
+			buffer.append(" ");
+			i++;
+		}
+		String activityTitleF = activityTitle + buffer;
+		t = 20 - description.length();
+		i = 0;
+		buffer.setLength(0);
+		while (i < t)
+		{
+			buffer.append(" ");
+			i++;
+		}
+		String descriptionF = description + buffer;
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+		return id + " " + startDate.format(formatter) + " " + endDate.format(formatter) + " " + activityTitleF
+				+ " " + descriptionF + " " + status;
 	}
 
-
+	/*
+	 * @Override public String toString() { DateTimeFormatter formatter =
+	 * DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm"); return id + " startDate: " +
+	 * startDate.format(formatter) + ", endDate: " + endDate.format(formatter) +
+	 * ", activityName: " + activityTitle + ", description: " + description +
+	 * ", status:" + status + ""; }
+	 */
 	public LocalDateTime getStartDate()
 	{
 		return startDate;
@@ -69,30 +96,27 @@ public class ToDoItem
 		return endDate;
 	}
 
-
 	public void setEndDate(LocalDateTime endDate)
 	{
 		this.endDate = endDate;
 	}
-
 
 	public Status getStatus()
 	{
 		return status;
 	}
 
-
 	public Boolean setStatus(Status status)
 	{
 		this.status = status;
 		return true;
 	}
-	
+
 	public Boolean isOverDue()
 	{
-		if ( LocalDateTime.now().isAfter(this.endDate))
+		if (LocalDateTime.now().isAfter(this.endDate))
 		{
-			this.status=Status.OVERDUE;
+			this.status = Status.OVERDUE;
 			return true;
 		}
 		else
@@ -103,6 +127,5 @@ public class ToDoItem
 	{
 		this.startDate = startDate;
 	}
-	
-	
+
 }
